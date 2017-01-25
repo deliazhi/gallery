382bdeb1fa478ebac79d287073074537d7c4c32b
zqf12069 <zqf12069@ly.com>
2017-01-25 10:46:10 +0800

    commit

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	title: 'Delia\'s Gallery',
+	wording: {
+		noAccess: '抱歉，你没有权限访问'
+	},
+	albums: {
+		"苏州": {
+		  description : "生活的地方",
+		  thumbnail : "DSC01296.jpg",
+		  name: "苏州"
+		},
+		"在人间": {
+		  description : "无需言语的故事",
+		  thumbnail : "",
+		  name: "在人间"
+		},
+		"私密": {
+		  description : "About Me",
+		  password:"1024",
+		  thumbnail : "",
+		  name: "私密"
+		},
+		"毕业": {
+		  description : "一时冲动，二月复习，三年坚持，四个学期，五位姐妹，六组同学，七易其稿，八仙过海，九张图片，十分满意",
+		  password:"1024",
+		  thumbnail : "WW0_7074.jpg",
+		  name: "毕业"
+		}
+	}
+}
